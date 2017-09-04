ef657f667694b0152fa7f7a65545783849d806f6
github username <github email address>
2017-09-04 10:29:24 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,6 +14,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  get 'students', to: 'student#index'
 
   # Example resource route with options:
   #   resources :products do
